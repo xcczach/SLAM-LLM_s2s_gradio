9c9cf6f0e66b0614ba2be941720b11928bb90818
xcc <2867389537@qq.com>
2024-10-12 22:27:38 +0800

    local run

--- app.py
+++ app.py
@@ -3,32 +3,36 @@ import soundfile as sf
 import os
 from s2s import generate
 
-def get_tmp_path(file_name:str):
+
+def get_tmp_path(file_name: str):
     return os.path.join("tmp", file_name)
 
+
 def process_audio(audio_file):
-    
+
     result_audio_arr, sample_rate = generate(audio_file)
-    
+
     target_path = get_tmp_path("processed_audio.wav")
     sf.write(target_path, result_audio_arr, sample_rate)
-    
+
     return target_path
+
 
 def text_to_audio(text):
     data, samplerate = sf.read("sample.wav")
-    
+
     processed_data = data
-    
+
     target_path = get_tmp_path("processed_text_audio.wav")
     sf.write(target_path, processed_data, samplerate)
-    
+
     return target_path
+
 
 with gr.Blocks() as demo:
     with gr.Row():
         gr.Markdown("### S2S")
-        audio_input = gr.Audio(label="Input Audio", type="filepath")
+        audio_input = gr.Audio(label="Input Audio", type="filepath", format="wav")
         audio_output = gr.Audio(label="Output Audio")
         audio_button = gr.Button("Start")
         audio_button.click(process_audio, inputs=audio_input, outputs=audio_output)
